7fe1de912b0442ee04f8fd27e5578991743547df
Travenedu <82552048+Travenedu@users.noreply.github.com>
2025-04-28 20:27:02 -0400

    Add lazygit

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,14 @@
+return {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open lazygit" },
+    },
+  }
+  
