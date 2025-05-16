1e845af8d3c10058b3f8e54b98144cbf0c5769de
Travenedu <82552048+Travenedu@users.noreply.github.com>
2025-05-16 17:04:59 -0400

    Modify Harpoon keymappings

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -27,17 +27,18 @@ return {
         })
   
         -- keymaps
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-  
-        vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() vim.notify("Harpooned! 🔔")
+        end, { desc = "Add file to harpoon list" })
+
+        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Open Harpoon Menu"})
+        vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
   
         -- Toggle previous & next
-        vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-        vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+        vim.keymap.set("n", "<C-h>", function() harpoon:list():prev() end)
+        vim.keymap.set("n", "<C-l>", function() harpoon:list():next() end)
   
         -- Telescope integration
         local conf = require("telescope.config").values
