55b95eccad557af41e68cc1c1ff6df0b2f829708
Travenedu <82552048+Travenedu@users.noreply.github.com>
2025-05-17 00:48:07 -0400

    cleanup init.lua

--- init.lua
+++ init.lua
@@ -14,11 +14,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Setup line numbers
--- vim.opt.number = true
--- vim.opt.relativenumber = true
--- vim.opt.signcolumn = "number"
-
 -- notify when yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
