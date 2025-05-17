0284a35d9fc2b50dd4777427bb97e9b22df5abbb
Travenedu <82552048+Travenedu@users.noreply.github.com>
2025-05-17 00:48:38 -0400

    Modify which-key configuration

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,25 +1,33 @@
 return {
 	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		opts = {
-			-- personal configuration here
+	  "folke/which-key.nvim",
+	  event = "VeryLazy",
+	  opts = {
+		-- your existing config here (optional)
+		plugins = {
+		  presets = {
+			operators = true,
+			motions = true,
+			text_objects = true,
+			windows = true,
+			nav = true,
+			z = true,
+			g = true,
+		  },
 		},
-		keys = {
-			{
-				"<leader>?",
-				function()
-					require("which-key").show({ global = false })
-				end,
-				desc = "Buffer Local Keymaps (which-key)",
-			},
-			{
-				"<leader>w",
-				function()
-					require("which-key").show({ global = true })
-				end,
-				desc = "Global Keymaps (which-key)",
-			},
+	  },
+	  keys = {
+		{
+		  "<leader>?",
+		  function() require("which-key").show({ global = false }) end,
+		  desc = "Buffer Local Keymaps (which-key)",
 		},
-	},
-}
+		{
+		  "<leader>w",
+		  function() require("which-key").show({ global = true }) end,
+		  desc = "Global Keymaps (which-key)",
+		},
+	  }
+	  
+  }
+} 
